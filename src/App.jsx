bcf6905fac6915d2605208973a3816c28640b28e
clodi <ciao@clodi.tech>
2024-02-05 17:08:15 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>a project by libero</header>
+      <main>
+        <div className='input'>input</div>
+        <div className='result'>results</div>
+      </main>
+    </>
+  )
+}
+
+export default App
