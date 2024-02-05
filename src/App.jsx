e2732a417032d09b91958801284b44d697e59ab8
verci-eth <ciao@sonolibero.io>
2024-02-05 18:00:05 +0100

    udpate font size

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [fontSize, setFontSize] = useState('0px');
+
+  const handleInputChange = (event) => {
+    setFontSize(`${event.target.value}px`);
+  }
 
   return (
     <>
       <header>a project by libero</header>
       <main>
-        <input className='input' type='number' placeholder='enter here the font size in px' autoFocus/>
-        <div className='result'>results</div>
+        <input
+          className='input'
+          type='number'
+          placeholder='enter here the font size in px'
+          autoFocus
+          onChange={handleInputChange}
+        />
+        <div className='result' style={{ fontSize: fontSize }}>results</div>
       </main>
     </>
   )
