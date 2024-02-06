571e043f42b4aa99612d37b13aec15962d550b05
clodi <ciao@clodi.tech>
2024-02-06 15:40:00 +0100

    Update font color and fix formatting in App.jsx and index.css

--- src/App.jsx
+++ src/App.jsx
@@ -17,27 +17,21 @@ function App() {
     <>
       <main>
         <h1 style={{ fontSize: `${calculateGoldenRatio(3)}px` }}>
-          {`<h1> ${calculateGoldenRatio(3).toFixed(2)}px`}
-        </h1>
+          {`<h1> ${calculateGoldenRatio(3).toFixed(2)}px`}</h1>
         <h2 style={{ fontSize: `${calculateGoldenRatio(2)}px` }}>
-          {`<h2> ${calculateGoldenRatio(2).toFixed(2)}px`}
-        </h2>
+          {`<h2> ${calculateGoldenRatio(2).toFixed(2)}px`}</h2>
         <h3 style={{ fontSize: `${calculateGoldenRatio(1)}px` }}>
-          {`<h3> ${calculateGoldenRatio(1).toFixed(2)}px`}
-        </h3>
+          {`<h3> ${calculateGoldenRatio(1).toFixed(2)}px`}</h3>
         <h4 style={{ fontSize: `${fontSize}px` }}>
-          {`<h4> ${fontSize}px`}
-        </h4>
+          {`<h4> ${fontSize}px`}</h4>
         <p style={{ fontSize: `${fontSize}px` }}>
-        {`<p> ${fontSize}px`}
-        </p>
+        {`<p> ${fontSize}px`}</p>
         <input
           type='range'
           min='5'
           max='20'
           value={fontSize}
-          onChange={handleInputChange}
-        />
+          onChange={handleInputChange}/>
       </main>
       <footer>a project by <a href="https://sonolibero.io" target='_blank'>libero</a></footer>
     </>
