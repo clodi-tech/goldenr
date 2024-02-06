3c9405ccf3fa42150792fa37ec62b87bc8153a16
verci-eth <ciao@sonolibero.io>
2024-02-06 15:50:19 +0100

    simplify

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 import './App.css'
 
-function Heading({ tag, multiplier, fontSize, calculateGoldenRatio }) {
+function Heading({ tag, multiplier, fontSize }) {
+  const goldenRatio = 1.61803399;
   const Tag = tag;
-  const size = calculateGoldenRatio(multiplier);
+  const size = fontSize * Math.pow(goldenRatio, multiplier);
 
   return (
     <Tag style={{ fontSize: `${size}px` }}>
@@ -14,15 +15,6 @@ function Heading({ tag, multiplier, fontSize, calculateGoldenRatio }) {
 
 function App() {
   const [fontSize, setFontSize] = useState(10);
-  const goldenRatio = 1.61803399;
-
-  const handleInputChange = (event) => {
-    setFontSize(event.target.value);
-  }
-
-  const calculateGoldenRatio = (multiplier) => {
-    return fontSize * Math.pow(goldenRatio, multiplier);
-  }
 
   return (
     <>
@@ -33,7 +25,6 @@ function App() {
             tag={tag}
             multiplier={3 - index}
             fontSize={fontSize}
-            calculateGoldenRatio={calculateGoldenRatio}
           />
         ))}
         <h4 style={{ fontSize: `${fontSize}px` }}>
@@ -45,7 +36,7 @@ function App() {
           min='5'
           max='20'
           value={fontSize}
-          onChange={handleInputChange}/>
+          onChange={(event) => setFontSize(event.target.value)}/>
       </main>
       <footer>a project by <a href="https://sonolibero.io" target='_blank'>libero</a></footer>
     </>
