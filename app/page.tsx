b9c3d0c0d8abe38d6ee9b25a4dbad581e8280fbc
clodi <ciao@clodi.tech>
2024-03-14 23:18:50 +0100

    migration

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,23 @@
-import Image from "next/image";
+'use client';
 
-export default async function Home() {
+import Image from "next/image";
+import { useState } from "react";
+
+function Heading({ tag, multiplier, fontSize }: { tag: string, multiplier: number, fontSize: number }) {
+  const goldenRatio = 1.61803399;
+  const Tag = tag;
+  const size = fontSize * Math.pow(goldenRatio, multiplier);
+
+  return (
+    <Tag style={{ fontSize: `${size}px` }}>
+      {`<${tag}> ${size.toFixed(2)}px`}
+    </Tag>
+  );
+}
+
+export default function Home() {
+  const [fontSize, setFontSize] = useState(15);
+
   return (
     <div className="parent">
       <header>
@@ -10,7 +27,23 @@ export default async function Home() {
             priority={true}/>
       </header>
       <div className="left-side"></div>
-      <main>main
+      <main>
+        {['h1', 'h2', 'h3'].map((tag, index) => (
+            <Heading
+              key={tag}
+              tag={tag}
+              multiplier={3 - index}
+              fontSize={fontSize}
+            />
+          ))}
+          <p style={{ fontSize: `${fontSize}px` }}>
+          {`<p> ${fontSize}px`}</p>
+          <input
+            type='range'
+            min='5'
+            max='30'
+            value={fontSize}
+            onChange={(event) => setFontSize(Number(event.target.value))}/>
       </main>
       <div className="right-side section yellow"></div>
       <footer>
