a519d5dd8c1bc2eb278d7981795e43f0a1263529
clodi <ciao@clodi.tech>
2024-04-09 15:30:46 +0200

    dynamic size

--- app/ui/rect.jsx
+++ app/ui/rect.jsx
@@ -1,40 +1,33 @@
 import { useState } from 'react';
 import { Slider } from '@nextui-org/slider';
 
-function Box({ gridColumn, gridRow, span, size }) {
+function SvgBox({ x, y, span, size }) {
     return (
-        <div className="border-1 border-gray-500"
-            style={{
-                gridColumn: `${gridColumn} / span ${span}`,
-                gridRow: `${gridRow} / span ${span}`,
-                width: `${span * size}px`,
-                height: `${span * size}px`
-            }}
-        />
+        <rect x={x} y={y} width={span * size} height={span * size} stroke="gray" fill="none" strokeWidth="1" />
     );
 }
 
-export default function Rect() {
+export default function RectSvg() {
     const [size, setSize] = useState(20);
 
     const boxes = [
-        { gridColumn: 1, gridRow: 1, span: 21 },
-        { gridColumn: 22, gridRow: 1, span: 13 },
-        { gridColumn: 22, gridRow: 14, span: 3 },
-        { gridColumn: 25, gridRow: 14, span: 2 },
-        { gridColumn: 25, gridRow: 16, span: 1 },
-        { gridColumn: 26, gridRow: 16, span: 1 },
-        { gridColumn: 22, gridRow: 17, span: 5 },
-        { gridColumn: 27, gridRow: 14, span: 8 },
+        { x: 0, y: 0, span: 21 }, // Adjust x, y to position each box
+        { x: 21 * size, y: 0, span: 13 },
+        { x: 21 * size, y: 13 * size, span: 3 },
+        { x: 24 * size, y: 13 * size, span: 2 },
+        { x: 24 * size, y: 15 * size, span: 1 },
+        { x: 25 * size, y: 15 * size, span: 1 },
+        { x: 21 * size, y: 16 * size, span: 5 },
+        { x: 26 * size, y: 13 * size, span: 8 },
     ];
 
     return (
         <main>
-            <div className="grid grid-cols-34 grid-rows-21">
+            <svg width={size * 34} height={size * 21}>
                 {boxes.map((box, index) => (
-                    <Box key={index} {...box} size={size} />
+                    <SvgBox key={index} {...box} size={size} />
                 ))}
-            </div>
+            </svg>
             <Slider aria-label="Volume" size="lg"
                 className='w-[15rem]'
                 value={size} onChange={setSize}
