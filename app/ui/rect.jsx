27f3314a3990abe2573aa8149cad242beb47e7e8
clodi <ciao@clodi.tech>
2024-04-09 14:38:49 +0200

    size as param

--- app/ui/rect.jsx
+++ app/ui/rect.jsx
@@ -1,16 +1,16 @@
-const size = 10;
+const size = 15;
 
 export default function Rect() {
     return (
         <main>
             <div className="grid grid-cols-13 grid-rows-21">
-                <div className="border-1 border-white w-130 h-130" style={{ gridColumn: "1 / span 13", gridRow: "1 / span 13" }}>13</div>
-                <div className="border-1 border-white w-30 h-30" style={{ gridColumn: "1 / span 3", gridRow: "14 / span 3" }}>3</div>
-                <div className="border-1 border-white w-20 h-20" style={{ gridColumn: "4 / span 2", gridRow: "14 / span 2" }}>2</div>
-                <div className="border-1 border-white w-10 h-10" style={{ gridColumn: 4, gridRow: 16 }}>1</div>
-                <div className="border-1 border-white w-10 h-10" style={{ gridColumn: 5, gridRow: 16 }}>1</div>
-                <div className="border-1 border-white w-50 h-50" style={{ gridColumn: "1 / span 5", gridRow: "17 / span 5" }}>5</div>
-                <div className="border-1 border-white w-80 h-80" style={{ gridColumn: "6 / span 8", gridRow: "14 / span 8" }}>8</div>
+                <div className="border-1 border-white" style={{ gridColumn: "1 / span 13", gridRow: "1 / span 13", width: `${13*size}px`, height: `${13*size}px` }}>13</div>
+                <div className="border-1 border-white" style={{ gridColumn: "1 / span 3", gridRow: "14 / span 3", width: `${3*size}px`, height: `${3*size}px` }}>3</div>
+                <div className="border-1 border-white" style={{ gridColumn: "4 / span 2", gridRow: "14 / span 2", width: `${2*size}px`, height: `${2*size}px` }}>2</div>
+                <div className="border-1 border-white" style={{ gridColumn: 4, gridRow: 16, width: `${size}px`, height: `${size}px` }}>1</div>
+                <div className="border-1 border-white" style={{ gridColumn: 5, gridRow: 16, width: `${size}px`, height: `${size}px` }}>1</div>
+                <div className="border-1 border-white" style={{ gridColumn: "1 / span 5", gridRow: "17 / span 5", width: `${5*size}px`, height: `${5*size}px` }}>5</div>
+                <div className="border-1 border-white" style={{ gridColumn: "6 / span 8", gridRow: "14 / span 8", width: `${8*size}px`, height: `${8*size}px` }}>8</div>
             </div>
         </main>
     );
