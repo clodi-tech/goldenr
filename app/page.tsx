f6cc937dd752cdfbbd909fe9db098d4f94737495
clodi <ciao@clodi.tech>
2024-04-09 18:04:31 +0200

    fixed a b

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,11 @@ export default function Home() {
     return (
         <main>
             <Tabs>
-                <Tab title="typography">
-                    <Typo />
-                </Tab>
                 <Tab title="space">
                     <Rect />
+                </Tab>
+                <Tab title="typography">
+                    <Typo />
                 </Tab>
             </Tabs>
         </main>
--- app/ui/rect.jsx
+++ app/ui/rect.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react';
 import { Slider } from '@nextui-org/slider';
+import Image from 'next/image';
+
+const fixed = 18;
+const gr = 1.61803399;
 
 function SvgBox({ x, y, span, size, rotate }) {
     const pathD = `M ${x} ${y} A ${span * size} ${span * size} 0 0 0 ${x + span * size} ${y + span * size}`;
@@ -16,30 +20,35 @@ function SvgBox({ x, y, span, size, rotate }) {
 }
 
 export default function RectSvg() {
-    const [size, setSize] = useState(20);
+    const [size, setSize] = useState(1024);
 
     const boxes = [
-        { x: 0 * size, y: 13 * size, span: 21, rotate: 0 },
-        { x: 0  * size, y: 0  * size, span: 13, rotate: 90 },
-        { x: 13 * size, y: 0  * size, span: 8, rotate: 180 },
-        { x: 16 * size, y: 8  * size, span: 5, rotate: -90 },
-        { x: 13 * size, y: 10 * size, span: 3, rotate: 0 },
-        { x: 13 * size, y: 8  * size, span: 2, rotate: 90 },
-        { x: 15 * size, y: 8  * size, span: 1, rotate: 180 },
-        { x: 15 * size, y: 9  * size, span: 1, rotate: -90 },
+        { x: 0  * fixed, y: 13 * fixed, span: 21, rotate: 0 },
+        { x: 0  * fixed, y: 0  * fixed, span: 13, rotate: 90 },
+        { x: 13 * fixed, y: 0  * fixed, span: 8 , rotate: 180 },
+        { x: 16 * fixed, y: 8  * fixed, span: 5 , rotate: -90 },
+        { x: 13 * fixed, y: 10 * fixed, span: 3 , rotate: 0 },
+        { x: 13 * fixed, y: 8  * fixed, span: 2 , rotate: 90 },
+        { x: 15 * fixed, y: 8  * fixed, span: 1 , rotate: 180 },
+        { x: 15 * fixed, y: 9  * fixed, span: 1 , rotate: -90 },
     ];
 
     return (
         <main>
-            <svg width={size * 21} height={size * 34} overflow='visible'>
-                {boxes.map((box, index) => (
-                    <SvgBox key={index} {...box} size={size} />
-                ))}
-            </svg>
+            <Image src="/formula.svg" alt='formula' width={142} height={50} />
+            <div className='flex gap-4'>
+                <p>a: {size}</p>
+                <p>b: {(size / gr).toFixed(5)}</p>
+            </div>
             <Slider aria-label="Volume" size="lg"
                 className='w-[15rem]'
                 value={size} onChange={setSize}
-                minValue={10} maxValue={30} />
+                minValue={1} maxValue={3000} />
+            <svg width={fixed * 21} height={fixed * 34} overflow='visible'>
+                {boxes.map((box, index) => (
+                    <SvgBox key={index} {...box} size={fixed} />
+                ))}
+            </svg>
         </main>
     );
 }
