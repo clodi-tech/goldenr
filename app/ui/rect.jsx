c223fce89ed6dd4debeb03d6ed55e04477668014
clodi <ciao@clodi.tech>
2024-04-09 13:24:18 +0200

    draw squares

--- app/ui/rect.jsx
+++ app/ui/rect.jsx
@@ -1,7 +1,23 @@
+const sizes = [ 1, 1, 2, 3, 5, 8, 13, 21 ];
+const unit = 10;
+
 export default function Rect() {
     return (
         <main>
-            <div style={{ width: '100px', height: '100px', backgroundColor: 'tomato' }}></div>
+            <div>
+            {sizes.map((size, index) => (
+                <div key={index}
+                    className="text-center"
+                    style={{
+                        width: `${size * unit}px`,
+                        height: `${size * unit}px`,
+                        border: '1px solid white',
+                        display: 'inline-block',
+                    }}>
+                    {size}
+                </div>
+            ))}
+            </div>
         </main>
-    )
+    );
 }
