41d98eb786d56eaeaff6d8946f3d41dd080ae5a3
clodi <ciao@clodi.tech>
2024-04-09 16:39:57 +0200

    rotate squares

--- app/ui/rect.jsx
+++ app/ui/rect.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 import { Slider } from '@nextui-org/slider';
 
-function SvgBox({ x, y, span, size }) {
+function SvgBox({ x, y, span, size, rotate }) {
     const pathD = `M ${x} ${y} A ${span * size} ${span * size} 0 0 0 ${x + span * size} ${y + span * size}`;
 
+    const centerX = x + (span * size) / 2;
+    const centerY = y + (span * size) / 2;
+
     return (
-        <g>
+        <g transform={`rotate(${rotate}, ${centerX}, ${centerY})`}>
             <rect x={x} y={y} width={span * size} height={span * size} stroke="gray" fill="none" strokeWidth="1" />
             <path d={pathD} stroke="white" fill="none" />
         </g>
@@ -16,14 +19,14 @@ export default function RectSvg() {
     const [size, setSize] = useState(20);
 
     const boxes = [
-        { x: 0  * size, y: 0  * size, span: 13 }, // Adjust x, y to position each box
-        { x: 13 * size, y: 0  * size, span: 8 },
-        { x: 13 * size, y: 8  * size, span: 2 },
-        { x: 15 * size, y: 8  * size, span: 1 },
-        { x: 15 * size, y: 9  * size, span: 1 },
-        { x: 16 * size, y: 8  * size, span: 5 },
-        { x: 13 * size, y: 10 * size, span: 3 },
-        { x: 0 * size, y: 13 * size, span: 21 },
+        { x: 0  * size, y: 0  * size, span: 13, rotate: 90 }, // Adjust x, y to position each box
+        { x: 13 * size, y: 0  * size, span: 8, rotate: 180 },
+        { x: 13 * size, y: 8  * size, span: 2, rotate: 90 },
+        { x: 15 * size, y: 8  * size, span: 1, rotate: 180 },
+        { x: 15 * size, y: 9  * size, span: 1, rotate: -90 },
+        { x: 16 * size, y: 8  * size, span: 5, rotate: -90 },
+        { x: 13 * size, y: 10 * size, span: 3, rotate: 0 },
+        { x: 0 * size, y: 13 * size, span: 21, rotate: 0 },
     ];
 
     return (
