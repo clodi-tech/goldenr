23d08b323818b49ed0649e4fd4b951ac62afe3fd
clodi <ciao@clodi.tech>
2024-04-09 14:28:39 +0200

    extend tailwind draw 8

--- app/ui/rect.jsx
+++ app/ui/rect.jsx
@@ -3,12 +3,14 @@ const size = 10;
 export default function Rect() {
     return (
         <main>
-            <div className="grid grid-cols-5 grid-rows-8">
+            <div className="grid grid-cols-13 grid-rows-8">
                 <div className="border-1 border-white w-30 h-30" style={{ gridColumn: "1 / span 3", gridRow: "1 / span 3" }}>3</div>
                 <div className="border-1 border-white w-20 h-20" style={{ gridColumn: "4 / span 2", gridRow: "1 / span 2" }}>2</div>
                 <div className="border-1 border-white w-10 h-10" style={{ gridColumn: 4, gridRow: 3 }}>1</div>
                 <div className="border-1 border-white w-10 h-10" style={{ gridColumn: 5, gridRow: 3 }}>1</div>
                 <div className="border-1 border-white w-50 h-50" style={{ gridColumn: "1 / span 5", gridRow: "4 / span 5" }}>5</div>
+                <div className="border-1 border-white w-80 h-80" style={{ gridColumn: "6 / span 8", gridRow: "1 / span 8" }}>8</div>
+
             </div>
         </main>
     );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,11 @@ const config: Config = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        '13': 'repeat(13, minmax(0, 1fr))',
+      },
+    },
   },
   darkMode: "class",
   plugins: [nextui()]
