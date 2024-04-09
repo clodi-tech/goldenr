7f7551c0465a181de92c753c36960da74c27dec9
clodi <ciao@clodi.tech>
2024-04-09 18:08:00 +0200

    improve

--- app/ui/rect.jsx
+++ app/ui/rect.jsx
@@ -4,23 +4,25 @@ import Image from 'next/image';
 
 const fixed = 18;
 const gr = 1.61803399;
+const width = 21 * fixed;
+const height = 34 * fixed;
 
-function SvgBox({ x, y, span, size, rotate }) {
-    const pathD = `M ${x} ${y} A ${span * size} ${span * size} 0 0 0 ${x + span * size} ${y + span * size}`;
+function SvgBox({ x, y, span, rotate }) {
+    const pathD = `M ${x} ${y} A ${span * fixed} ${span * fixed} 0 0 0 ${x + span * fixed} ${y + span * fixed}`;
 
-    const centerX = x + (span * size) / 2;
-    const centerY = y + (span * size) / 2;
+    const centerX = x + (span * fixed) / 2;
+    const centerY = y + (span * fixed) / 2;
 
     return (
         <g transform={`rotate(${rotate}, ${centerX}, ${centerY})`}>
-            <rect x={x} y={y} width={span * size} height={span * size} stroke="gray" fill="none" strokeWidth="1" />
+            <rect x={x} y={y} width={span * fixed} height={span * fixed} stroke="gray" fill="none" strokeWidth="1" />
             <path d={pathD} stroke="white" fill="none" strokeWidth="2" />
         </g>
     );
 }
 
 export default function RectSvg() {
-    const [size, setSize] = useState(1024);
+    const [base, setBase] = useState(1024);
 
     const boxes = [
         { x: 0  * fixed, y: 13 * fixed, span: 21, rotate: 0 },
@@ -35,18 +37,18 @@ export default function RectSvg() {
 
     return (
         <main>
-            <Image src="/formula.svg" alt='formula' width={142} height={50} />
+            <Image src="/formula.svg" alt='formula' width={142} height={50} priority />
             <div className='flex gap-4'>
-                <p>a: {size}</p>
-                <p>b: {(size / gr).toFixed(5)}</p>
+                <p>a: {base}</p>
+                <p>b: {(base / gr).toFixed(5)}</p>
             </div>
             <Slider aria-label="Volume" size="lg"
                 className='w-[15rem]'
-                value={size} onChange={setSize}
+                value={base} onChange={setBase}
                 minValue={1} maxValue={3000} />
-            <svg width={fixed * 21} height={fixed * 34} overflow='visible'>
+            <svg width={width} height={height} overflow='visible'>
                 {boxes.map((box, index) => (
-                    <SvgBox key={index} {...box} size={fixed} />
+                    <SvgBox key={index} {...box} />
                 ))}
             </svg>
         </main>
