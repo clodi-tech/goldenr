3c6024483a5df6bbd64095d58085b33355fadc13
clodi <ciao@clodi.tech>
2024-04-09 14:45:13 +0200

    with slider

--- app/ui/rect.jsx
+++ app/ui/rect.jsx
@@ -1,18 +1,27 @@
-const size = 15;
+'use client'
+
+import { useState } from 'react'
+import { Slider } from '@nextui-org/slider'
 
 export default function Rect() {
+    const [size, setSize] = useState(20);
+
     return (
         <main>
             <div className="grid grid-cols-34 grid-rows-21">
-                <div className="border-1 border-white" style={{ gridColumn: "1 / span 21", gridRow: "1 / span 21", width: `${21*size}px`, height: `${21*size}px` }}>21</div>
-                <div className="border-1 border-white" style={{ gridColumn: "22 / span 13", gridRow: "1 / span 13", width: `${13*size}px`, height: `${13*size}px` }}>13</div>
-                <div className="border-1 border-white" style={{ gridColumn: "22 / span 3", gridRow: "14 / span 3", width: `${3*size}px`, height: `${3*size}px` }}>3</div>
-                <div className="border-1 border-white" style={{ gridColumn: "25 / span 2", gridRow: "14 / span 2", width: `${2*size}px`, height: `${2*size}px` }}>2</div>
-                <div className="border-1 border-white" style={{ gridColumn: 25, gridRow: 16, width: `${size}px`, height: `${size}px` }}>1</div>
-                <div className="border-1 border-white" style={{ gridColumn: 26, gridRow: 16, width: `${size}px`, height: `${size}px` }}>1</div>
-                <div className="border-1 border-white" style={{ gridColumn: "22 / span 5", gridRow: "17 / span 5", width: `${5*size}px`, height: `${5*size}px` }}>5</div>
-                <div className="border-1 border-white" style={{ gridColumn: "27 / span 8", gridRow: "14 / span 8", width: `${8*size}px`, height: `${8*size}px` }}>8</div>
+                <div className="border-1 border-white" style={{ gridColumn: "1 / span 21", gridRow: "1 / span 21", width: `${21*size}px`, height: `${21*size}px` }} />
+                <div className="border-1 border-white" style={{ gridColumn: "22 / span 13", gridRow: "1 / span 13", width: `${13*size}px`, height: `${13*size}px` }} />
+                <div className="border-1 border-white" style={{ gridColumn: "22 / span 3", gridRow: "14 / span 3", width: `${3*size}px`, height: `${3*size}px` }} />
+                <div className="border-1 border-white" style={{ gridColumn: "25 / span 2", gridRow: "14 / span 2", width: `${2*size}px`, height: `${2*size}px` }} />
+                <div className="border-1 border-white" style={{ gridColumn: 25, gridRow: 16, width: `${size}px`, height: `${size}px` }} />
+                <div className="border-1 border-white" style={{ gridColumn: 26, gridRow: 16, width: `${size}px`, height: `${size}px` }} />
+                <div className="border-1 border-white" style={{ gridColumn: "22 / span 5", gridRow: "17 / span 5", width: `${5*size}px`, height: `${5*size}px` }} />
+                <div className="border-1 border-white" style={{ gridColumn: "27 / span 8", gridRow: "14 / span 8", width: `${8*size}px`, height: `${8*size}px` }} />
             </div>
+            <Slider aria-label="Volume" size="lg"
+                className='w-[15rem]'
+                value={size} onChange={setSize}
+                minValue={10} maxValue={30}/>
         </main>
     );
 }
